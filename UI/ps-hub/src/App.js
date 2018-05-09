d46be3a2c55903dfddae8ce017bf0e0dcc6c69af
AkshayNalawade <singhharpreet547@gmail.com>
2018-05-09 10:55:16 +0530

    psHUB UI: Updated gitignore file

--- UI/ps-hub/src/App.js
+++ UI/ps-hub/src/App.js
@@ -5,11 +5,10 @@ import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-//import NavBar from './components/navBar/NavBar';
 import Login from './components/login/Login';
 import SignUp from './components/signUp/SignUp';
 import NavBarContainer from './redux/containers/NavBarContainer';
-//<NavBar {...this.props}/>
+
 class App extends Component {
   render() {
     return (
