1a5a78d14446d36faeb71936d8f3e18689cd84d3
AkshayNalawade <singhharpreet547@gmail.com>
2018-07-31 16:00:24 +0530

    changes in admin, ui and server

--- UI/ps-hub/src/redux/Store.js
+++ UI/ps-hub/src/redux/Store.js
@@ -1,9 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
+// import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(
-    rootReducer
-    //+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-console.log(store.getState());
+const store = createStore(rootReducer, composeWithDevTools(
+  //applyMiddleware(...middleware),
+  // other store enhancers if any
+));
+
 export default store;
